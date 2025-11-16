898e7be1b52baace0c72c84b0d7222d51f938f8d
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2025-11-16 14:21:19 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ResponseLogger } from "@/components/response-logger";
+import { cookies } from "next/headers";
+import { ReadyNotifier } from "@/components/ready-notifier";
+import FarcasterWrapper from "@/components/FarcasterWrapper";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const requestId = cookies().get("x-request-id")?.value;
+
+  return (
+        <html lang="en">
+          <head>
+            {requestId && <meta name="x-request-id" content={requestId} />}
+          </head>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {/* Do not remove this component, we use it to notify the parent that the mini-app is ready */}
+            <ReadyNotifier />
+            
+      <FarcasterWrapper>
+        {children}
+      </FarcasterWrapper>
+      
+            <ResponseLogger />
+          </body>
+        </html>
+      );
+}
+
+export const metadata: Metadata = {
+        title: "ZamaFHE Connect",
+        description: "Build and deploy a decentralized app using FHE technology on Vercel. Easily sync with GitHub and access a detailed README for seamless integration.",
+        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_da36423f-51b7-4e2b-abf5-6799be48afa8-Yva04U6Pr4Szw1VMJKg9REYz0j7rCd","button":{"title":"Open with Ohara","action":{"type":"launch_frame","name":"ZamaFHE Connect","url":"https://each-fifteen-624.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
+        ) }
+    };
